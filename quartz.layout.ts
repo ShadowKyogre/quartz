e35405d4dc18307a8bfe523e2c1f559f1d7bc028
ShadowKyogre <shadowkyogre.public@gmail.com>
2024-06-19 22:16:48 -0700

    Add common configs for layout
    - We do not want a graph.
    - We do want to omit dummy indexes from the filebrowser (and sortspec).

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -13,10 +13,31 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+export const commonConfigs = {
+    Breadcrumbs: {
+      showCurrentPage: true
+    },
+    Explorer: {
+        folderClickBehavior: "link",
+        filterFn: (node) => {
+            // we want to skip over the index file at the root...
+            if (node.children == 0 && node.file == null) {
+                return false;
+            }
+
+            if (node.name === 'sortspec') {
+                return false;
+            }
+
+            return true;
+        },
+    },
+}
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs(commonConfigs.Breadcrumbs),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -26,10 +47,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer(commonConfigs.Explorer)),
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
@@ -37,13 +58,13 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(commonConfigs.Breadcrumbs), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer(commonConfigs.Explorer)),
   ],
   right: [],
 }
